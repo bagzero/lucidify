3fc214ac1c28e439cb4024afb713bb2a71305c06
Ayush <135484695+bagzero@users.noreply.github.com>
2024-11-26 15:38:22 -0500

    Update page.tsx

--- app/dashboard/projects/[projectId]/page.tsx
+++ app/dashboard/projects/[projectId]/page.tsx
@@ -15,7 +15,6 @@ type ProjectPageProps = {
 
 const ProjectPage = ({ params }: ProjectPageProps) => {
   const router = useRouter();
-  const { user, loading } = useAuth();  // Use your auth hook
   const searchParams = useSearchParams(); // Initialize useSearchParams
   const userId = searchParams.get('userId'); // Get userId from query parameters
   const projectId = searchParams.get('projectId'); // Get projectId from query parameters
@@ -23,19 +22,11 @@ const ProjectPage = ({ params }: ProjectPageProps) => {
   console.log(userId)
   console.log(projectId)
   useEffect(() => {
-    if (!loading) {
-      if (!user) {
-        router.push('/login');  // Redirect to login if no user is logged in
-      } else {
-        // Check if the user is admin based on their email
-        if (user.email === 'ayush.bhujle@gmail.com') {
-          // Admin
-        } else {
-          // Client
-        }
-      }
-    }
-  }, [user, loading, router]);
+  if (!user) {
+    router.push('/login'); // Redirect to login if no user is logged in
+  }
+}, [user, router]);
+
 
   // Ensure userId and projectId exist before rendering the component
   if (!userId || !projectId) {
