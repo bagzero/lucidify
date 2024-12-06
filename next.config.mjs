59358b53e965f6055b4b198592c57e4d8912c5d8
Ayush <135484695+bagzero@users.noreply.github.com>
2024-12-06 05:01:25 -0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com'], // Add Google's image domain here
-    },
-  };
-  
-  export default nextConfig;
-  
