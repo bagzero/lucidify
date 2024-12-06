a08ef3fc654dfea8607ffde2e069fda22f9afceb
Ayush <135484695+bagzero@users.noreply.github.com>
2024-12-06 05:05:25 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'], // Add Google's image domain here
+    },
+  };
+  
+  export default nextConfig;
+  
